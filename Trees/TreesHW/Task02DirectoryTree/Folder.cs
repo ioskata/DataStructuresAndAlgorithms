40ef0242fd33ed9451bbd33feccdf832b076bff0
ttitto <ttitto_prog@vfemail.net>
2015-07-26 23:03:05 +0300

    Task 02 Directory Tree solved

---
+++ Trees/TreesHW/Task02DirectoryTree/Folder.cs
@@ -0,0 +1,31 @@
+﻿namespace Task02DirectoryTree
+{
+    using System.Collections.Generic;
+
+    public class Folder
+    {
+        private ICollection<File> files;
+        private ICollection<Folder> folders;
+
+        public Folder(string name)
+        {
+            this.Name = name;
+            this.Files = new HashSet<File>();
+            this.Folders = new HashSet<Folder>();
+        }
+
+        public string Name { get; set; }
+
+        public ICollection<File> Files
+        {
+            get {return this.files; }
+            set { this.files = value; }
+        }
+
+        public ICollection<Folder> Folders
+        {
+            get { return this.folders; }
+            set { this.folders = value; }
+        }
+    }
+}
---
+++ Trees/TreesHW/Task02DirectoryTree/Task02DirectoryTreeClass.cs
@@ -0,0 +1,54 @@
+﻿namespace Task02DirectoryTree
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class Task02DirectoryTreeClass
+    {
+        public static void Main(string[] args)
+        {
+            const string startFolderPath = @"C:\Windows";
+            Folder startFolder = new Folder(startFolderPath);
+            BuildTree(startFolder);
+
+            Console.WriteLine(GetFolderSize(startFolder));
+        }
+
+        private static void BuildTree(Folder folder)
+        {
+            Console.WriteLine(folder.Name);
+            var files = new DirectoryInfo(folder.Name).GetFiles();
+            foreach (var childFile in files)
+            {
+                File file = new File(childFile.FullName, childFile.Length);
+                folder.Files.Add(file);
+            }
+
+            var folders = new DirectoryInfo(folder.Name).GetDirectories();
+            foreach (var childFolder in folders)
+            {
+                Folder customFolder = new Folder(childFolder.FullName);
+                folder.Folders.Add(customFolder);
+                BuildTree(customFolder);
+            }
+        }
+
+        public static long GetFolderSize(Folder rootFolder)
+        {
+            if (rootFolder == null)
+            {
+                return 0;
+            }
+
+            long childSum = 0;
+
+            foreach (var folder in rootFolder.Folders)
+            {
+                childSum += GetFolderSize(folder);
+            }
+
+            return childSum + rootFolder.Files.Sum(f => f.Size);
+        }
+    }
+}
